0555cc7a40079b6a318801ed638a2601ebe93900
BManussanan <150109233+BManussanan@users.noreply.github.com>
2024-08-20 17:31:53 +0700

    Update 204.py

---
+++ lab2/204.py
@@ -0,0 +1,10 @@
+print("First fraction:")
+a = int(input(">>Enter a numerator a: "))
+b = int(input(">>Enter a denominator b: "))
+print("Second fraction:")
+c = int(input(">>Enter a numerator c: "))
+d = int(input(">>Enter a denominator d: "))
+sum1 = a*d+c*b
+sum2 = b*d
+output = "Summation of the two fractions is {0} / {1}".format(sum1,sum2)
+print(output)
