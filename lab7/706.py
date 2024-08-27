9dde41a6d49a2026e15955a42aa0b4bc7f59f58e
BManussanan <150109233+BManussanan@users.noreply.github.com>
2024-08-28 01:22:48 +0700

    Update 706.py

---
+++ lab7/706.py
@@ -0,0 +1,41 @@
+def star(wid,x):
+    j = 0
+    while j < wid:
+        if x%2 != 0:
+            print('* ',end='')
+        else:
+            print(' *',end='')
+        j += 1
+
+h = int(input('Enter height: '))
+w = int(input('Enter width: '))
+if h <= 0 or w <= 0:
+    print('Invalid input, program terminates.')
+else:
+    i = 1
+    while i <= h:
+        star(w,i)
+        print()
+        i += 1
+
+#ทำได้สองแบบ
+def star(wid):
+    j = 0
+    out = ''
+    while j < wid:
+        out += '* '
+        j += 1
+    return out
+
+h = int(input('Enter height: '))
+w = int(input('Enter width: '))
+if h <= 0 or w <= 0:
+    print('Invalid input, program terminates.')
+else:
+    i = 1
+    while i <= h:
+        if i%2 != 0:
+            print(star(w))
+        else:
+            print(f' {star(w)}')
+        i += 1
